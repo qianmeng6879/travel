e7a59db52e135af8ef4712333f8a8adfb980bfc1
zero <qianmeng6879@163.com>
2022-10-22 10:47:03 +0800

    配置全局异常处理

--- src/main/java/top/mxzero/travel/advice/GlobalExceptionHandler.java
+++ src/main/java/top/mxzero/travel/advice/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package top.mxzero.travel.advice;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -10,9 +15,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @since 2022/9/28
  */
 //@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
-    public String exceptionHandler(Exception e) {
-        return null;
+    public Object exceptionHandler(Exception e) {
+        LOGGER.error(e.getMessage());
+        Map<String,Object> map = new HashMap<>();
+        map.put("error", e.getMessage());
+        map.put("type", e.getClass().getName());
+
+        return map;
     }
 }
