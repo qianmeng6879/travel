c2e5e0938fbb264a0447a9b70d42506ae2b863ca
zero <qianmeng6879@163.com>
2022-11-28 09:28:28 +0800

    spring security加载中文消息

--- src/main/java/top/mxzero/travel/config/ReloadMessageConfig.java
+++ src/main/java/top/mxzero/travel/config/ReloadMessageConfig.java
@@ -1,17 +1,25 @@
 package top.mxzero.travel.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import java.util.Locale;
+
 //security中文提示信息配置类
 @Configuration
 public class ReloadMessageConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReloadMessageConfig.class);
+
     @Bean //加载中文认证提示信息
-    public ReloadableResourceBundleMessageSource messageSource(){
+    public ReloadableResourceBundleMessageSource messageSource() throws Exception {
+        Locale.setDefault(Locale.CHINA);
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         //加载org/springframework/security包下的中文提示信息 配置文件
         messageSource.setBasename("classpath:org/springframework/security/messages_zh_CN");
+
         return messageSource;
     }
 }
